abaa1c540955ddf45082e42904074a1472dc704f
Frederik Luartes <fjluartes@gmail.com>
2026-06-18 15:31:07 +0800

    08 adds calcSlope

--- src/defs.h
+++ src/defs.h
@@ -8,10 +8,15 @@
 #define SCREEN_WIDTH  1280
 #define SCREEN_HEIGHT 720
 
+#define FPS 60
+
 #define PLAYER_SPEED 5
 #define PLAYER_BULLET_SPEED 20
+#define ALIEN_BULLET_SPEED 10
 
 #define MAX_KEYBOARD_KEYS 350
 
 #define SIDE_PLAYER 0
 #define SIDE_ALIEN 1
+
+#define MAX_STARS 500
