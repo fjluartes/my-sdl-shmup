7b7689cfe79a9304be859bcfdf801dda10602fd3
Fred W11 <fjluartes@gmail.com>
2026-06-20 15:14:56 +0800

    11 adds text.c

---
+++ src/text.c
@@ -0,0 +1,18 @@
+/*
+ * text.c: File to render text
+ */
+#include "common.h"
+
+#include "draw.h"
+#include "text.h"
+
+
+void initFonts(void)
+{
+
+}
+
+void drawText(int x, int y, int r, int g, int b, char *format, ...)
+{
+    
+}
