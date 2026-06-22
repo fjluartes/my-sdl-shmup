addf530664b1b701111c9fa292c0082462902f8d
Frederik Luartes <fjluartes@gmail.com>
2026-06-22 14:44:59 +0800

    13 edits draw.c for addTextureToCache

--- src/draw.c
+++ src/draw.c
@@ -20,11 +20,45 @@ void presentScene(void)
 	SDL_RenderPresent(app.renderer);
 }
 
+static SDL_Texture *getTexture(char *name)
+{
+    Texture *t;
+
+    for (t = app.textureHead.next; t != NULL; t = t->next)
+    {
+        if (strcmp(t->name, name) == 0)
+        {
+            return t->texture;
+        }
+    }
+    return NULL;
+}
+
+static void addTextureToCache(char *name, SDL_Texture *sdlTexture)
+{
+    Texture *texture;
+
+    texture = malloc(sizeof(Texture));
+    memset(texture, 0, sizeof(Texture));
+    app.textureTail->next = texture;
+    app.textureTail = texture;
+
+    STRNCPY(texture->name, name, MAX_NAME_LENGTH);
+    texture->texture = sdlTexture;
+}
+
 SDL_Texture *loadTexture(char *filename)
 {
     SDL_Texture *texture;
-    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
-    texture = IMG_LoadTexture(app.renderer, filename);
+
+    texture = getTexture(filename);
+
+    if (texture == NULL)
+    {
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
+        texture = IMG_LoadTexture(app.renderer, filename);
+        addTextureToCache(filename, texture);    
+    }
 
     return texture;
 }
@@ -36,7 +70,7 @@ void blit(SDL_Texture *texture, int x, int y)
     dest.x = x;
     dest.y = y;
     SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
-    // change dest.w, dest.h to resize
+
     SDL_RenderCopy(app.renderer, texture, NULL, &dest);
 }
 
